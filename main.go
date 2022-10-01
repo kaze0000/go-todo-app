5d81ce68ca73f125211aa1dd46a30259b4d0387b
kaze0000 <kazuya.00000.ds25@gmail.com>
2022-10-01 21:36:26 +0900

    todoの取得

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main(){
 
 	fmt.Println(models.Db) //テーブルの作成
 
-	//ユーザーの作成
+	/*ユーザーの作成
 	u := &models.User{}
 	u.Name = "test"
 	u.Email = "test@example.com"
@@ -25,6 +25,7 @@ func main(){
 	fmt.Println(u)
 
 	u.CreateUser()
+	*/
 
 	/*
 	u, _ := models.GetUser(1)
@@ -41,6 +42,11 @@ func main(){
 	fmt.Println(u)
 	*/
 
+	/*
 	user, _ := models.GetUser(2)
 	user.CreateTodo("First Todo")
+	*/
+
+	t, _ := models.GetTodo(1)
+	fmt.Println(t)
 }
