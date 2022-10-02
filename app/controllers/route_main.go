908330d4a40b0b9bf09aeac30dbe338f1f7aa0c3
kaze0000 <kazuya.00000.ds25@gmail.com>
2022-10-02 16:53:48 +0900

    layoutの共通化

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -1,15 +1,16 @@
 package controllers
 
 import (
-	"fmt"
 	"net/http"
-	"text/template"
 )
 
 func top(w http.ResponseWriter, _  *http.Request) { //ハンドラー
+	/*
 	t, err := template.ParseFiles("app/views/templates/top.html")
 	if err != nil {
 		fmt.Println(err)
 	}
 	t.Execute(w, "hello")
+	*/
+	generateHTML(w, "hello", "layout", "top")
 }
